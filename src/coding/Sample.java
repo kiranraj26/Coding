db1894a13c30e479591f82ffa1a2c65eaadc356b
kiranraj26 <kiranrajv26@gmail.com>
2025-02-27 12:08:02 +0530

    Update Sample.java

--- src/coding/Sample.java
+++ src/coding/Sample.java
@@ -5,6 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		//ystem.out.println("Hi");commits in code
+		
 
 	}
 
